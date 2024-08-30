9d0f8a3add2b726ae9956c3fac9336d86ab48c83
Nicola Major <nicola@major.travel>
2024-08-30 08:48:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 $(document).ready(function() {
     const animals = ['lion', 'elephant', 'giraffe', 'zebra', 'rhino', 'hippo', 'cheetah', 'leopard', 'buffalo', 'ostrich'];
-    
-    let spinCount = 0;
 
     function getRandomAnimal() {
         return animals[Math.floor(Math.random() * animals.length)];
     }
 
-    function spinReel(reelId) {
-        const animal = getRandomAnimal();
-        // Ensure the file path points to the correct directory
-        $(reelId).css('background-image', `url(images/${animal}.png)`);
-        return animal;
+    function spinReel(reelId, callback) {
+        $(reelId).animate({ top: '-150px' }, 500, function() {
+            const animal = getRandomAnimal();
+            $(reelId).css('background-image', `url(images/${animal}.jpg)`);
+            $(reelId).css('top', '0px'); // Reset position after animation
+            if (callback) callback(animal);
+        });
     }
 
     $('#spinButton').click(function() {
-        spinCount++;
         $('#message').text('');
 
-        const result1 = spinReel('#reel1');
-        const result2 = spinReel('#reel2');
-        const result3 = spinReel('#reel3');
-
-        // Check if the spin count is divisible by 3 or if all results match
-        if (spinCount % 3 === 0) {
-            $('#reel1').css('background-image', `url(images/${result1}.png)`);
-            $('#reel2').css('background-image', `url(images/${result1}.png)`);
-            $('#reel3').css('background-image', `url(images/${result1}.png)`);
-            $('#message').text('You are a winner!');
-        } else if (result1 === result2 && result2 === result3) {
-            $('#message').text('You are a winner!');
-        }
+        spinReel('#reel1', function(result1) {
+            spinReel('#reel2', function(result2) {
+                spinReel('#reel3', function(result3) {
+                    if (result1 === result2 && result2 === result3) {
+                        $('#message').text('You are a winner!');
+                    } else {
+                        $('#message').text('Try again!');
+                    }
+                });
+            });
+        });
     });
 });
